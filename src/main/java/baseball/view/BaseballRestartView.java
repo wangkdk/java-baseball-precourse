94cd8f3ef33b73228d73870ceda5fef5a92c0308
Wangki Lee <lwk3007@gmail.com>
2021-10-04 20:57:58 +0900

    feat: 게임 종료 재시작 view
    BaseballRestartView
    - 입력값 검증

---
+++ src/main/java/baseball/view/BaseballRestartView.java
@@ -0,0 +1,38 @@
+package baseball.view;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import nextstep.utils.Console;
+
+public class BaseballRestartView {
+
+	private final static String RESTART_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private final static String VALIDATE_RESTART_INPUT_MESSAGE = "[ERROR] 잘못된 문자를 입력하였습니다. 다시 입력해주세요.";
+	private final static Map<String, Boolean> selectOption = new HashMap<>();
+
+	static {
+		selectOption.put("1", true);
+		selectOption.put("2", false);
+	}
+
+	public static boolean selectRestart() {
+		System.out.println(RESTART_INPUT_MESSAGE);
+		String inputSelect = Console.readLine();
+		try {
+			return validateInputSelect(inputSelect);
+		} catch (IllegalArgumentException exception) {
+			System.out.println(exception.getMessage());
+			return selectRestart();
+		}
+	}
+
+	private static boolean validateInputSelect(String inputSelect) {
+		Boolean isRestart = selectOption.get(inputSelect);
+		if (Objects.isNull(isRestart)) {
+			throw new IllegalArgumentException(VALIDATE_RESTART_INPUT_MESSAGE);
+		}
+		return isRestart;
+	}
+}
