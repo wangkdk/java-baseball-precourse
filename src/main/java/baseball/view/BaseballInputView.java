c4d1bd9cfe96686b870a7f2f546b8f5524ac64a4
Wangki Lee <lwk3007@gmail.com>
2021-10-04 15:17:35 +0900

    feat: 플레이어 숫자 입력 view

---
+++ src/main/java/baseball/view/BaseballInputView.java
@@ -0,0 +1,20 @@
+package baseball.view;
+
+import baseball.domain.BaseballNumber;
+import nextstep.utils.Console;
+
+public class BaseballInputView {
+
+	private final static String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요: ";
+
+	public static BaseballNumber inputBaseballNumber() {
+		System.out.print(INPUT_NUMBER_MESSAGE);
+		String inputNumbers = Console.readLine();
+		try {
+			return BaseballNumber.create(inputNumbers);
+		} catch (IllegalArgumentException exception) {
+			System.out.println(exception.getMessage());
+			return inputBaseballNumber();
+		}
+	}
+}
