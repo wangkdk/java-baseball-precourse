21d848ab12da8ceff5cf294b25c9226ac3be62c9
Wangki Lee <lwk3007@gmail.com>
2021-10-04 21:01:08 +0900

    feat: 게임 재시작 기능

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -4,6 +4,7 @@ import baseball.domain.BaseballNumber;
 import baseball.domain.BaseballNumberGenerator;
 import baseball.domain.BaseballResult;
 import baseball.view.BaseballInputView;
+import baseball.view.BaseballRestartView;
 import baseball.view.BaseballResultView;
 
 public class BaseballController {
@@ -17,5 +18,12 @@ public class BaseballController {
 			BaseballResultView.printMatchResult(matchResult);
 			isAnswer = matchResult.isPerfect();
 		} while (!isAnswer);
+		reStart();
+	}
+
+	public static void reStart() {
+		if (BaseballRestartView.selectRestart()) {
+			start();
+		}
 	}
 }
