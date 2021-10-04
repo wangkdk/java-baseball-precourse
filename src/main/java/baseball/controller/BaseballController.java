85fc6c18cf0d557641b7ab632f26e5295fa929bb
Wangki Lee <lwk3007@gmail.com>
2021-10-04 18:57:02 +0900

    feat: 게임 시작 기능

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,21 @@
+package baseball.controller;
+
+import baseball.domain.BaseballNumber;
+import baseball.domain.BaseballNumberGenerator;
+import baseball.domain.BaseballResult;
+import baseball.view.BaseballInputView;
+import baseball.view.BaseballResultView;
+
+public class BaseballController {
+
+	public static void start() {
+		BaseballNumber baseballNumber = BaseballNumber.create(BaseballNumberGenerator.generateNonDuplicateNumbers());
+		boolean isAnswer;
+		do {
+			BaseballNumber playerBaseballNumber = BaseballInputView.inputBaseballNumber();
+			BaseballResult matchResult = baseballNumber.match(playerBaseballNumber);
+			BaseballResultView.printMatchResult(matchResult);
+			isAnswer = matchResult.isPerfect();
+		} while (!isAnswer);
+	}
+}
